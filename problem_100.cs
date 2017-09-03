a7a1e4777043cb403d5df0ec9ad3df5e965746a0
Igor Subbotin <moat1981@gmail.com>
2017-09-04 01:26:39 +0300

    Solve Problem 100 - Same Tree

---
+++ problem_100.cs
@@ -0,0 +1,18 @@
+// 100. Same Tree - https://leetcode.com/problems/same-tree
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
