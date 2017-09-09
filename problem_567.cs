28569c56f852d39505245f148171652be2853191
Igor Subbotin <moat1981@gmail.com>
2017-09-09 16:17:03 +0300

    Solve Problem 567 - Permutation in String

--- problem_567.cs
+++ problem_567.cs
@@ -2,19 +2,12 @@
 public class Solution {
     public bool CheckInclusion(string s1, string s2) {
         var hash = new int[256];
-        foreach (var c in s1) {
-            hash[c]++;
-        }
-        var source = hash.ToList().ToArray();
+        foreach (var c in s1) hash[c]++;
         var left = 0;
         var right = 0;
         var count = s1.Length;
         while (right < s2.Length) {
-            hash[s2[right]]--;
-            if (hash[s2[right]] >= 0) {
-                count--;
-            }
-            right++;
+            if (--hash[s2[right++]] >= 0) count--;
             if (count == 0) return true;
             if (right - left == s1.Length && ++hash[s2[left++]] > 0) count++;
         }
