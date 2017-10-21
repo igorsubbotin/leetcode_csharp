dcbd2d9b49b961319095ddaebde76ba15842397e
Igor Subbotin <moat1981@gmail.com>
2017-10-22 01:28:01 +0300

    Solve Problem 094 - Binary Tree Inorder Traversal

--- problem_094.cs
+++ problem_094.cs
@@ -11,24 +11,16 @@
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
         var result = new List<int>();
-        if (root == null) return result;
-        var q = new Queue<TreeNode>();
-        q.Enqueue(root);
-        var right = new Stack<TreeNode>();
-        while (q.Count > 0 || right.Count > 0) {
-            while (q.Count > 0) {
-                var node = q.Dequeue();
-                if (node.left == null && node.right == null) result.Add(node.val);
-                else {
-                    if (node.left != null) q.Enqueue(node.left);
-                    right.Push(node);
-                }
-            }
-            while (right.Count > 0) {
-                var node = right.Pop();
-                if (node.right != null) q.Enqueue(node.right); 
+        var stack = new Stack<TreeNode>();
+        var p = root;
+        while (stack.Count > 0 || p != null) {
+            if (p != null) {
+                stack.Push(p);
+                p = p.left;
+            } else {
+                var node = stack.Pop();
                 result.Add(node.val);
-                break;
+                p = node.right;
             }
         }
         return result;
