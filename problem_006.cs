319ee129bada519def4fbe83520b21316deee057
Igor Subbotin <moat1981@gmail.com>
2017-10-28 02:06:34 +0300

    Solve Problem 006 - ZigZag Conversion

--- problem_006.cs
+++ problem_006.cs
@@ -2,24 +2,23 @@
 public class Solution {
     public string Convert(string s, int numRows) {
         if (numRows == 1) return s;
-        var result = new StringBuilder();
-        var step = GetStepSize(numRows);
-        for (var i = 0; i < numRows; i++) {
-            var j = i;
-            var substep = GetStepSize(numRows - i);
-            while (j < s.Length) {
-                result.Append(s[j]);
-                if (i != 0 && i != numRows - 1) {
-                    var k = j + substep;
-                    if (k < s.Length) result.Append(s[k]);
-                }
-                j += step;
-            }
+        var result = new List<StringBuilder>();
+        for (var j = 0; j <= numRows; j++)
+            result.Add(new StringBuilder());
+        var i = 0;
+        var increase = true;
+        var row = 1;
+        while (i < s.Length) {
+            result[row].Append(s[i]);
+            if (increase) row++;
+            else row--;
+            if (row == 1) increase = true;
+            else if (row == numRows) increase = false;
+            i++;
         }
-        return result.ToString();
-    }
-    
-    private static int GetStepSize(int numRows) {
-        return (numRows - 2) * 2 + 2;
+        var sb = new StringBuilder();
+        foreach (var each in result)
+            sb.Append(each.ToString());
+        return sb.ToString();
     }
 }
