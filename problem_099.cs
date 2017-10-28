f660ca9268bdf0509594d4f57bc61e8e1b303ca3
Igor Subbotin <moat1981@gmail.com>
2017-10-28 15:49:04 +0300

    Solve Problem 099 - Recover Binary Search Tree

--- problem_099.cs
+++ problem_099.cs
@@ -11,18 +11,25 @@
 public class Solution {
     public void RecoverTree(TreeNode root) {
         if (root == null) return;
-        var list = new List<TreeNode>();
-        Traverse(root, list);
-        var sorted = list.OrderBy(x => x.val).Select(x => x.val).ToList();
-        for (var i = 0; i < list.Count; i++) {
-            list[i].val = sorted[i];
-        }
+        TreeNode prev = null;
+        TreeNode first = null;
+        TreeNode second = null;
+        Traverse(root, ref prev, ref first, ref second);
+        var tmp = first.val;
+        first.val = second.val;
+        second.val = tmp;
     }
     
-    private static void Traverse(TreeNode root, IList<TreeNode> list) {
+    private static void Traverse(TreeNode root, ref TreeNode prev, ref TreeNode first, ref TreeNode second) {
         if (root == null) return;
-        Traverse(root.left, list);
-        list.Add(root);
-        Traverse(root.right, list);
+        Traverse(root.left, ref prev, ref first, ref second);
+        if (prev != null) {
+            if (prev.val > root.val) {
+                if (first == null) first = prev;
+                second = root;
+            }
+        }
+        prev = root;
+        Traverse(root.right, ref prev, ref first, ref second);
     }
 }
